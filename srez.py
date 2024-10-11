3b7b94c57ad67d4ad5b5db48e34af4364b50c331
eg1oria <leontevegor57@gmail.com>
2024-10-11 16:34:17 +0500

    Add files via upload

---
+++ srez.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([2, 3, 4])
+b = np.array([3, 4, 5])
+c = np.array(a + b)
+d = np.array(2 * a)
+e = np.array(3 * b)
+
+print(d)
+print(e)
+
+c1 = np.array(2 * a + 3 * b)
+print(c1)
